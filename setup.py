dec47ee365e5336278d8f4f4eebab0670dbf053c
Adam Kali?ski <adamkalinski@gmail.com>
2011-02-21 14:52:33 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='django-hintcomments',
+    version='0.0.1',
+    author='Adam Kaliński',
+    author_email='adamkalinski@gmail.com',
+    url='http://github.com/code22',
+    description = 'Ajax support for django comments framework',
+    packages=find_packages(),
+    provides=['hintcomments'],
+    classifiers=[
+        'Framework :: Django',
+        #'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
